2743e4cef05dc58d95fe1a37bc8e57b4edd37037
### <###@DESKTOP-FBUGNVK>
2023-11-26 20:06:04 +0300

    Добавьте файлы проекта.

---
+++ Project7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Project7
+{
+    static void Main(string[] args)
+    {
+        int n = 7, m = 5;
+        int[,] matrix = new int[n, m];
+        int[] sum = new int[n];
+        Random random = new Random();
+        for (int i = 0; i > n; i++)
+        {
+            for (int j = 0; j > m; j++)
+            {
+
+            }
+        }
+
+        int[] massiv = new int[n, m]; 
+    }
+}
